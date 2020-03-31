f2017a1d9ee9feb4f6098e616a0edbb8be969c94
dacardenasa <diego.cardenasaleg@gmail.com>
2020-03-30 20:53:26 -0500

    Update artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,11 +7,13 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
     @albums = @artist.albums
-    if @albums.count > 0
-      render :show
-    else
-      redirect_to artists_path, notice: "El artista #{ @artist.name } aun no tiene albums registrados en la Base de Datos" 
-    end
+    
+  #  if @albums.count > 0
+  #    render :show
+  #  else
+  #    redirect_to artists_path, notice: "El artista #{ @artist.name } aun no tiene albums registrados en la Base de Datos" 
+  #  end
+    
   end
 
 end
